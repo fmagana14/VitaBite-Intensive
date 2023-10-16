c55acbed723e34960db6543c1896406ec76e6ae8
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-10-16 15:03:57 -0700

    completed Bar graph and JS file

--- script.js
+++ script.js
@@ -6,19 +6,45 @@ function updateSliderValue(id, value){
 const calorieSliders = document.querySelectorAll ('.calorie-slider');
 const calorieChart = document.getElementById ('calorie-chart');
 
-console.log(calorieSliders)
 calorieSliders.forEach(slider => {
+    console.log(slider)
     slider.addEventListener('input', updateCalories);
+    slider.addEventListener('change', updateCalories)
 })
 
 function updateCalories() {
     const totalCalories = Array.from(calorieSliders).reduce((total, slider) => total + parseInt(slider.value), 0);
-    document.getElementById('calorie-chart').style.width = (totalCalories / 2000 * 100) + '%';
+    console.log(totalCalories)
+    document.getElementById('calorie-chart').style.width = (totalCalories / 4000 * 100) + '%';
 }
 
 function open(tabContent){
     var i, x;
-    x = docutment.getElementbyClassName("nav-item")
+    x = docutment.getElementbyClassName("nav-item");
 
 }
 
+
+function addFood() {
+    const mealSelector = document.getElementById("meals");
+    const selectedMeal = mealSelector.value;
+    const foodInput = document.getElementById("site-search");
+    const caloriesInput = parseInt(foodInput.value); 
+
+    const spanId = `calorie-${selectedMeal.toLowerCase()}-slider`;
+    const span = document.getElementById(spanId);
+    console.log(spanId) 
+    
+    const sliderId = `${selectedMeal.toLowerCase()}-slider`;
+    const slider = document.getElementById(sliderId)
+    console.log(sliderId)
+    slider.value = caloriesInput;
+
+    updateCalories()
+
+
+    const caloriesDisplay = document.getElementById(`calorie-${selectedMeal.toLowerCase()}-slider`);
+    caloriesDisplay.textContent = caloriesInput;
+
+    foodInput.value = "";
+}
